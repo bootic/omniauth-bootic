7973514bef748da34e431539f4abc1676d62221f
Ismael Celis <ismaelct@gmail.com>
2014-06-17 19:43:08 +0100

    User info is not in the API's root.

--- lib/omniauth/strategies/bootic.rb
+++ lib/omniauth/strategies/bootic.rb
@@ -3,10 +3,10 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Bootic < OmniAuth::Strategies::OAuth2
-      
+
       BOOTIC_AUTH_URL = (ENV['BOOTIC_AUTH_URL'] || "https://auth.bootic.net")
       BOOTIC_API_URL  = (ENV['BOOTIC_API_URL'] || "https://api.bootic.net/v1")
-      
+
       # Give your strategy a name.
       option :name, "Bootic"
       option :provider_ignores_state, true
@@ -18,11 +18,11 @@ module OmniAuth
         authorize_url: "#{BOOTIC_AUTH_URL}/oauth/authorize",
         token_url: "#{BOOTIC_AUTH_URL}/oauth/token"
       }
-      
+
       def request_phase
         super
       end
-      
+
       def authorize_params
         if request.params['scope']
           super.merge({scope: request.params['scope']})
@@ -30,7 +30,7 @@ module OmniAuth
           super
         end
       end
-      
+
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
       # additional calls (if the user id is returned with the token
@@ -53,9 +53,9 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/v1/me').parsed
+        @raw_info ||= access_token.get('/v1').parsed
       end
-      
+
     end
   end
 end
